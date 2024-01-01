a1322814fcd093450870aa0f2670ff6cd850ad41
mohit <mohitt0704@gmaiil.com>
2024-01-01 16:35:18 +0530

    second commit

---
+++ arrays-04/src/com/mohit/Input.java
@@ -0,0 +1,58 @@
+package com.mohit;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // array of primitives
+        int[] arr = new int[5];
+        arr[0] = 32;
+        arr[1] = 44;
+        arr[2] = 56;
+        arr[3] = 12;
+        arr[4] = 445;
+        System.out.println(arr[0]);
+
+        // input using for Loop;
+//        for(int i=0; i<arr.length; i++){
+//            arr[i] = in.nextInt();
+    }
+//        for(int i=0; i<arr.length; i++){
+//            System.out.print(arr[i] + " ");
+//        }
+
+        // using forEach loop;
+//        for(int num : arr){
+//            System.out.print(num + "");  //here num represent the element of the array
+//        }
+
+        // easy way to print this;
+//            System.out.println(Arrays.toString(arr));
+
+        // Array of objects
+       //strings
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ arrays-04/src/com/mohit/multiDimension.java
@@ -0,0 +1,40 @@
+package com.mohit;
+
+public class multiDimension {
+    public static void main(String[] args) {
+        /*
+          1 2 3
+          4 5 6
+          7 8 9
+         */
+
+//        int[][] arr = new int[3][];
+
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 3},
+                {2, 54, 4, 3}
+        };
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
