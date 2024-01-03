a56cf46400b9b161f85b665f6d66b9501f2128d5
mohit <mohitt0704@gmaiil.com>
2024-01-03 15:32:13 +0530

    second commit

---
+++ arrays-04/src/com/mohit/arrayListt.java
@@ -0,0 +1,16 @@
+package com.mohit;
+
+import java.util.ArrayList;
+
+public class arrayListt {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(5);
+        list.add(22);
+        list.add(321);
+        list.add(232);
+        list.add(3423);
+        list.add(32);
+        list.add(323);
+        System.out.println(list);
+    }
+}
